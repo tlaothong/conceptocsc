8105531aacf7fd31da8fea0d7cd313a7e95f82c4
Perawatt <pekung4433@perfenterprise.com>
2016-01-20 20:21:11 +0700

    Implement UI Certificated and CertificatedDetail

---
+++ src/Complementary/RegistrationWeb/Controllers/CertificatedController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationWeb.Controllers
+{
+    public class CertificatedController : Controller
+    {
+        // GET: Certificated
+        public ActionResult Certificated()
+        {
+            return View();
+        }
+        public ActionResult CertificatedDetail()
+        {
+            return View();
+        }
+    }
+}
