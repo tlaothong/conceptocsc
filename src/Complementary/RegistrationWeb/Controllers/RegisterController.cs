1c25e3147acc2c949886c8949201ef7acf786480
earn <earndarkness@perfenterprise.com>
2016-01-20 15:56:54 +0700

    Implement register module
    Signed-off-by: earn <earndarkness@perfenterprise.com>

---
+++ src/Complementary/RegistrationWeb/Controllers/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationWeb.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Register personal detail
+        public ActionResult PersonalDetail()
+        {
+            return View();
+        }
+    }
+}
