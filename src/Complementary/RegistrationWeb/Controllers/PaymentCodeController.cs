aff9b1748ae19cfdfe427d0296586c7ef4967103
Perawatt <pekung4433@perfenterprise.com>
2016-01-20 17:28:51 +0700

    implement UI PaymentCode

---
+++ src/Complementary/RegistrationWeb/Controllers/PaymentCodeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationWeb.Controllers
+{
+    public class PaymentCodeController : Controller
+    {
+        // GET: PaymentCode
+        public ActionResult PaymentCode()
+        {
+            return View();
+        }
+    }
+}
